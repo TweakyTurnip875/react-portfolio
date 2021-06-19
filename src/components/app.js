adda5ed60e2aa4cc1820807227254f537303d139
TweakyTurnip875 <noahmsmith2006@gmail.com>
2021-06-18 18:14:04 -0600

    Portfolio item / container

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import moment from 'moment';
 
+import PortfolioContainer from './portfolio/portfolio-container';
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Noah Smith portfolio</h1>
+        
         <div>
-          <h3>{moment().format('MMMM Do YYYY, h:mm:ss a')}</h3>
+          {moment().format('MMMM Do YYYY, h:mm:ss a')}
         </div>
+        <PortfolioContainer />
       </div>
     );
   }
---
+++ src/components/portfolio/portfolio-container.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+import PortfolioItem from "./portfolio-item";
+
+export default class PortfolioContainer extends Component {
+    constructor() {
+        super()
+        console.log("Portfolio container has rendered.")
+    }
+    portfolioItems() {
+        const data = [1, 2, 3, 4]
+
+        return data.map(item => {
+            return <PortfolioItem title={item} url="google.com" />
+        })
+    }
+    render() {
+        return(
+            <div>
+                <h2>Portfolio Items go here...</h2>
+                {this.portfolioItems()}
+            </div>
+        )
+    }
+}

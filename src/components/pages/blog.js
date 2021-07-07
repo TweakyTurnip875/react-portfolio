77449c244430cb888b73701bd2e4ebcea37258d4
TweakyTurnip875 <noahmsmith2006@gmail.com>
2021-07-07 15:05:56 -0600

    Blog component gets blogs from api and stores them in state.

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,13 +1,46 @@
-import React from "react";
-import { Link } from "react-router-dom"
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-export default function (props) {
-  return (
-    <div>
-      <h2>blog</h2>
-      <div>
-        <Link to="/about">Read More</Link>
-      </div>
-    </div>
-  );
+class Blog extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			blogItems: [],
+		};
+
+		this.getBlogItems = this.getBlogItems.bind(this);
+	}
+
+	getBlogItems() {
+		axios
+			.get("https://tweakyturnip875.devcamp.space/portfolio/portfolio_blogs", {
+				withCredentials: true,
+			})
+			.then((res) => {
+        this.setState({
+          blogItems: res.data.portfolio_blogs
+        })
+      })
+      .catch(error => {
+        console.log(error)
+      })
+	}
+
+  componentWillMount() {
+    this.getBlogItems()
+  }
+	render() {
+		return (
+			<div>
+				<h2>blog</h2>
+				<div>
+					<Link to="/about">Read More</Link>
+				</div>
+			</div>
+		);
+	}
 }
+
+export default Blog;

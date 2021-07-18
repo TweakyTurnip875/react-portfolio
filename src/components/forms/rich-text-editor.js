79d0b283ed14cda14715c8105a98fb6ea6107027
TweakyTurnip875 <noahmsmith2006@gmail.com>
2021-07-18 13:37:28 -0600

    Customized scrollbar and added base image form upload setup.

--- src/components/forms/rich-text-editor.js
+++ src/components/forms/rich-text-editor.js
@@ -1,35 +1,54 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { EditorState, convertToRaw } from 'draft-js';
 import { Editor } from 'react-draft-wysiwyg';
 import draftToHtml from 'draftjs-to-html';
 import htmlToDraft from 'html-to-draftjs';
 
 export default class RichTextEditor extends Component {
-    constructor(props) {
-        super(props)
+	constructor(props) {
+		super(props);
 
-        this.state = {
-            editorState: EditorState.createEmpty()
-        }
-        this.onEditorStateChange = this.onEditorStateChange.bind(this)
-    }
-    onEditorStateChange(editorState) {
-        this.setState({
-            editorState
-        }, this.props.handleRichTextEditorChange(
-            draftToHtml(convertToRaw(this.state.editorState.getCurrentContent()))
-        ))
-    }
-    render() {
-        return (
-            <div>
-                <Editor
-                    editorState={this.state.editorState}
-                    wrapperClassName="demo-wrapper"
-                    editorClassName="demo-editor"
-                    onEditorStateChange={this.onEditorStateChange}
-                />
-            </div>
-        )
-    }
+		this.state = {
+			editorState: EditorState.createEmpty(),
+		};
+		this.onEditorStateChange = this.onEditorStateChange.bind(this);
+	}
+	uploadFile(file) {
+		console.log(file);
+	}
+	onEditorStateChange(editorState) {
+		this.setState(
+			{
+				editorState,
+			},
+			this.props.handleRichTextEditorChange(
+				draftToHtml(convertToRaw(this.state.editorState.getCurrentContent()))
+			)
+		);
+	}
+	render() {
+		return (
+			<div>
+				<Editor
+					editorState={this.state.editorState}
+					wrapperClassName="demo-wrapper"
+					editorClassName="demo-editor"
+					onEditorStateChange={this.onEditorStateChange}
+					toolbar={{
+						inline: { inDropdown: true },
+						list: { inDropdown: true },
+						textAlign: { inDropdown: true },
+						link: { inDropdown: true },
+						history: { inDropdown: true },
+						image: {
+							uploadCallback: this.uploadFile,
+							alt: { present: true, manditory: false },
+							previewImage: true,
+							inputAccept: 'image/gif,image/jpeg,image/jpg,image/png,image/svg',
+						},
+					}}
+				/>
+			</div>
+		);
+	}
 }
